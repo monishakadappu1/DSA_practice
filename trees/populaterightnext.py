03c24d85099de08c6897d590ee484ededd6b18b8
Monisha Kadappu <40620335+monishakadappu1@users.noreply.github.com>
2020-05-18 13:14:34 +0530

    https://leetcode.com/problems/populating-next-right-pointers-in-each-node
    medium

--- trees/populaterightnext.py
+++ trees/populaterightnext.py
@@ -31,3 +31,30 @@ class Solution:
         self.connectnext(root.left,level+1)
         self.connectnext(root.right,level+1)
         return root
+
+    
+#using constant space ,best solution
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+        return self.connectNext(root,None)
+    
+    def connectNext(self,root,nextroot):
+        if root is None:
+            return None
+        root.next = nextroot
+        self.connectNext(root.left,root.right)
+        self.connectNext(root.right,root.next.left if root.next else None)
+        return root
+        

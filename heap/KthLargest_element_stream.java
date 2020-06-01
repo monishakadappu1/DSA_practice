427258eefaab6928a39e24153dc97f9848899f84
Monisha Kadappu <40620335+monishakadappu1@users.noreply.github.com>
2020-06-01 12:57:08 +0530

    updated with priority queue

--- heap/KthLargest_element_stream.java
+++ heap/KthLargest_element_stream.java
@@ -78,3 +78,51 @@ class KthLargest {
  
  //this code is suited if array size is fixed ,but since its a stream of integer we need to use something felxible like queue .the next code
  //is implemented in priority queue
+
+class KthLargest {
+    PriorityQueue<Integer> pQueue;
+    int count;
+    
+    public KthLargest(int k, int[] nums) {
+        count = k;
+        pQueue = new PriorityQueue<Integer>(count);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i<count)
+            {
+                pQueue.add(nums[i]);
+            }
+            else if(i>=count)
+            {
+                int val = pQueue.peek();
+                if(val<nums[i])
+                {
+                    pQueue.poll();
+                    pQueue.add(nums[i]);
+                }
+            }
+        }
+    }
+    
+    public int add(int val) {
+        if(pQueue.size()<count)
+        {
+            pQueue.add(val);
+        }
+        else{
+           int data = pQueue.peek();
+            if(data<val)
+            {
+                pQueue.poll();
+                pQueue.add(val);
+            } 
+        }
+        return pQueue.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */

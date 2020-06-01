32df3a271173f642c281ec163130dc9c93b5d2e1
Monisha Kadappu <40620335+monishakadappu1@users.noreply.github.com>
2020-06-01 12:36:16 +0530

    https://leetcode.com/problems/kth-largest-element-in-a-stream/
    easy

---
+++ heap/KthLargest_element_stream.java
@@ -0,0 +1,80 @@
+class KthLargest {
+    int count;
+    int[] nums;
+    int counter;
+    public KthLargest(int k, int[] numsarr) {
+       count = k;
+        nums = new int[numsarr.length];
+        nums = numsarr; 
+        int counter = 0;
+    }
+    
+    public int add(int val) {
+        if(counter==0)
+        {
+            int i=0;
+            while(i<nums.length){
+                if(i<count)
+                {
+                    minheapify(i);
+                }
+                if(i>=count)
+                {
+                    if(nums[0]<nums[i])
+                    {
+                        nums[0] = nums[i];
+                        minheapify(0);
+                    }
+                }
+                i++;
+            }
+            if(nums.length==0)
+            {
+                nums = new int[]{val};
+            }
+            counter=1;
+        }
+        if(val>nums[0])
+        {
+            nums[0]=val;
+            minheapify(0);
+        }
+        return nums[0];
+    }
+    
+    public void minheapify(int indx)
+    {
+        
+        int parent = indx;
+        int lc = 2*indx+1;
+        int rc = 2*indx+2;
+        int largest = -1;
+        if(lc<count && lc<nums.length && nums[lc]<nums[parent])
+        {
+            largest = lc;
+        }
+        else if(rc<count && lc<nums.length && nums[rc]<nums[parent])
+        {
+            largest = rc;
+        }
+        
+        if(largest!=-1)
+        {
+            int temp = nums[parent];
+            nums[parent] = nums[largest];
+            nums[largest] = temp;
+            parent = largest;
+            minheapify(parent);
+        }
+        return;
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+ 
+ //this code is suited if array size is fixed ,but since its a stream of integer we need to use something felxible like queue .the next code
+ //is implemented in priority queue
